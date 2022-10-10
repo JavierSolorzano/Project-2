70ecec9707a643c3c8b782b84ccc412f085308a3
David-0928668 <112572063+David-0928668@users.noreply.github.com>
2022-10-10 08:54:21 -0700

    Update AdminHousingStats.h

--- AdminHousingStats.h
+++ AdminHousingStats.h
@@ -1,20 +1,25 @@
-//Javier Solorzano & David Saldana
+//David Saldana
 #include <string>
 using namespace std;
 
 //defines this once to prevent redefiniton when the header file is included
 #pragma once
 
+struct RegionStats{
+int firstYear;
+int lastYear;//both used to find year num
+int yearNum;//size of our dynamic arrays using year range
+string fileName;//holds the name of region
+};
+
 class AdminHousingStats
 {
 private:
-int firstYear;//both used read year range
-//for default program data in txt files
-int lastYear;
-int yearNum;//size of our dynamic arrays using year range
-string fileName;//holds the name of region
+
+RegionStats currentRegion;
+
 const string txT = ".txt";//appends to the end of fileName variable 
-//to open file for said region
+  
 float *vacancyRate;//dynamic array to hold vacancy rate
 float *avgHomeValue;//dynamic array to hold average home price 
 float *homeownershipRate;//dynamic array to hold homeownership rate
@@ -22,12 +27,12 @@ float *homeownershipRate;//dynamic array to hold homeownership rate
 public:
 
 AdminHousingStats(){//Default constructor/initialization
-  firstYear = 0;
-  lastYear = 0; 
-  yearNum = 0;
-  vacancyRate = new float(yearNum);
-  avgHomeValue = new float(yearNum);
-  homeownershipRate = new float(yearNum);
+  currentRegion.firstYear = 0;
+  currentRegion.lastYear = 0; 
+  currentRegion.yearNum = 0;
+  vacancyRate = new float(currentRegion.yearNum);
+  avgHomeValue = new float(currentRegion.yearNum);
+  homeownershipRate = new float(currentRegion.yearNum);
 }
 void createRegion();//user can create a region
  
@@ -35,7 +40,6 @@ void updateRegionStats();//update a region's individual stats
 
 void displayRegionData(string nameOfFile);//displays data for specific region
 void setFileName();//updates the name the text file for the region
-//void deleteRegion();
 int menuLoop();//main menu system of the program
 void updateRegionYears();//user updates the year range for a specific region
 ~AdminHousingStats(){//frees memory for dynamic arrays
